cd1f57824bcde66bee0a030f7ec7180afabdcf89
dzgtr <tomas.merth@atlas.cz>
2023-03-18 15:04:02 +0100

    localization prep

--- src/cardb/settings.py
+++ src/cardb/settings.py
@@ -106,7 +106,12 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-GB'
+
+LANGUAGES = (
+    ("en_GB", "English"),
+    ("cs_CZ", "Čeština"),
+)
 
 TIME_ZONE = 'UTC'
 
